c20b516991da7dafff2ffbcf1aae57a28b648d62
shubham sharma <shubhamsharma01504@gmail.com>
2023-11-22 22:13:06 +0530

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by shubham sharma on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
