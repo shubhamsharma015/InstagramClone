13871fce08b4322e67de613f3515fb233bad662b
shubham sharma <shubhamsharma01504@gmail.com>
2023-12-10 21:16:31 +0530

    Added HomeView

---
+++ InstagramClone/HomePage/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  InstagramClone
+//
+//  Created by shubham sharma on 22/11/23.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
---
+++ InstagramClone/SearchPage/SearchPageView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPageView.swift
+//  InstagramClone
+//
+//  Created by shubham sharma on 22/11/23.
+//
+
+import SwiftUI
+
+struct SearchPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPageView()
+}
